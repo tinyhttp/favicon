b4092c98078c598ad4135e113d35ae2fd3a4526b
v1rtl <pilll.pl22@gmail.com>
2021-07-07 18:57:15 +0300

    feat: move favicon to separate repo

--- README.md
+++ README.md
@@ -8,15 +8,47 @@
 
 Description.
 
+> A rewrite of [serve-favicon](https://github.com/expressjs/serve-favicon) module.
+
+Node.js middleware to serve `favicon.ico` file.
+
 ## Install
 
 ```sh
-pnpm i pkg
+pnpm i @tinyhttp/favicon
 ```
 
-[npm-url]: https://npmjs.com/package/lru-send
-[github-actions]: https://github.com/tinyhttp/lru-send/actions
-[gh-actions-img]: https://img.shields.io/github/workflow/status/tinyhttp/tinyhttp/CI?style=for-the-badge&logo=github&label=
-[cov-img]: https://img.shields.io/coveralls/github/tinyhttp/lru-send?style=for-the-badge
-[cov-url]: https://coveralls.io/github/tinyhttp/lru-send
-[npm-img]: https://img.shields.io/npm/dt/lru-send?style=for-the-badge
+## API
+
+```js
+import { favicon } from '@tinyhttp/favicon'
+```
+
+### Options
+
+`favicon` accepts these properties in the options object.
+
+#### `path`
+
+Path to icon, required. Passed as the first argument.
+
+#### `maxAge`
+
+Sets `Cache-Control: maxAge=` header, optional. Default is one year. Passed with object in the second argument.
+
+## Example
+
+```js
+import { favicon } from '@tinyhttp/favicon'
+import { createServer } from 'http'
+import path from 'path'
+
+http.createServer(favicon(path.join(process.cwd(), 'public', 'favicon.ico')).listen(3000)
+```
+
+[npm-url]: https://npmjs.com/package/@tinyhttp/favicon
+[github-actions]: https://github.com/tinyhttp/favicon/actions
+[gh-actions-img]: https://img.shields.io/github/workflow/status/tinyhttp/favicon/CI?style=for-the-badge&logo=github&label=
+[cov-img]: https://img.shields.io/coveralls/github/tinyhttp/favicon?style=for-the-badge
+[cov-url]: https://coveralls.io/github/tinyhttp/favicon
+[npm-img]: https://img.shields.io/npm/dt/@tinyhttp/favicon?style=for-the-badge
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,15 +1,182 @@
-import { suite } from 'uvu'
-import * as assert from 'uvu/assert'
-import { add } from '../src/index'
+import { Context, suite, uvu } from 'uvu'
+import { favicon, FaviconOptions } from '../src/index'
+import http from 'http'
+import path from 'path'
+import expect from 'expect'
+import { makeFetch } from 'supertest-fetch'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-function describe(name: string, fn: (...args: any[]) => void) {
+function describe(name: string, fn: (it: uvu.Test<Context>) => void) {
   const s = suite(name)
   fn(s)
   s.run()
 }
 
-describe('add', (it) => {
-  it('adds', () => {
-    assert.equal(add(4, 5), 9)
+const FAVICON_PATH = path.join(process.cwd(), 'tests/fixtures/favicon.ico')
+
+function createServer(path?: string | Buffer, opts?: Omit<FaviconOptions, 'path'>) {
+  const _path = path || FAVICON_PATH
+  const _favicon = favicon(_path, opts)
+  const server = http.createServer(
+    async (req, res) =>
+      void _favicon(req, res, (err) => {
+        res.statusCode = err ? err.status || 500 : 404
+        res.end(err ? err.message : 'oops')
+      })
+  )
+
+  return server
+}
+
+describe('favicon function test', () => {
+  describe('args', () => {
+    describe('path', (it) => {
+      it('should accept buffer', async () => {
+        const server = createServer(Buffer.alloc(20))
+
+        await makeFetch(server)('/favicon.ico').expect(200)
+      })
+      it('should not be dir', async () => {
+        try {
+          favicon(__dirname)
+        } catch (e) {
+          expect((e as NodeJS.ErrnoException).message).toMatch(/EISDIR, illegal operation on directory/)
+        }
+      })
+      it('should exist', async () => {
+        try {
+          favicon('nothing')
+        } catch (e) {
+          expect((e as NodeJS.ErrnoException).message).toMatch(/ENOENT/)
+        }
+      })
+    })
+
+    describe('options.maxAge', function (it) {
+      it('should be in cache-control', async () => {
+        const server = createServer(null, { maxAge: 5000 })
+        await makeFetch(server)('/favicon.ico').expect('Cache-Control', 'public, max-age=5').expect(200)
+      })
+
+      it('should have a default', async () => {
+        const server = createServer()
+        await makeFetch(server)('/favicon.ico')
+          .expect('Cache-Control', /public, max-age=[0-9]+/)
+          .expect(200)
+      })
+
+      it('should accept 0', async () => {
+        const server = createServer(null, { maxAge: 0 })
+        await makeFetch(server)('/favicon.ico').expect('Cache-Control', 'public, max-age=0').expect(200)
+      })
+
+      it('should accept string', async () => {
+        const server = createServer(null, { maxAge: '30d' })
+        await makeFetch(server)('/favicon.ico').expect('Cache-Control', 'public, max-age=2592000').expect(200)
+      })
+
+      it('should be valid delta-seconds', async () => {
+        const server = createServer(null, { maxAge: 1234 })
+        await makeFetch(server)('/favicon.ico').expect('Cache-Control', 'public, max-age=1').expect(200)
+      })
+    })
+
+    describe('requests', (it) => {
+      let server: http.Server
+
+      it.before.each(() => {
+        server = createServer()
+      })
+
+      it('should serve icon', async () => {
+        await makeFetch(server)('/favicon.ico').expect('Content-Type', 'image/x-icon').expect(200)
+      })
+
+      it('should include cache-control', async () => {
+        await makeFetch(server)('/favicon.ico')
+          .expect('Cache-Control', /public/)
+          .expect(200)
+      })
+
+      it('should include strong etag', async () => {
+        await makeFetch(server)('/favicon.ico')
+          .expect('ETag', /^"[^"]+"$/)
+          .expect(200)
+      })
+
+      it('should deny POST', async () => {
+        await makeFetch(server)('/favicon.ico', {
+          method: 'POST'
+        })
+          .expect('Allow', 'GET, HEAD, OPTIONS')
+          .expect(405)
+      })
+
+      it('should understand OPTIONS', async () => {
+        await makeFetch(server)('/favicon.ico', {
+          method: 'OPTIONS'
+        })
+          .expect('Allow', 'GET, HEAD, OPTIONS')
+          .expect(200)
+      })
+
+      it('should 304 when If-None-Match matches', async () => {
+        const res = await makeFetch(server)('/favicon.ico').expect(200)
+
+        await makeFetch(server)('/favicon.ico', {
+          headers: {
+            'If-None-Match': res.headers.get('etag')
+          }
+        }).expect(304)
+      })
+
+      it('should 304 when If-None-Match matches weakly', async () => {
+        const res = await makeFetch(server)('/favicon.ico').expect(200)
+        await makeFetch(server)('/favicon.ico', {
+          headers: {
+            'If-None-Match': 'W/' + res.headers.get('etag')
+          }
+        }).expect(304)
+      })
+
+      it('should ignore non-favicon requests', async () => {
+        await makeFetch(server)('/').expect(404, 'oops')
+      })
+
+      it('should work with query string', async () => {
+        await makeFetch(server)('/favicon.ico?v=1').expect('Content-Type', 'image/x-icon').expect(200)
+      })
+    })
+
+    describe('icon', function () {
+      describe('buffer', (it) => {
+        it('should be served from buffer', async () => {
+          const buffer = Buffer.alloc(20, '#')
+          const server = createServer(buffer)
+
+          await makeFetch(server)('/favicon.ico')
+            .expectHeader('Content-Type', 'image/x-icon')
+            .expectHeader('Content-Length', '20')
+            .expectBody(buffer.toString())
+        })
+
+        it('should be copied', async () => {
+          const buffer = Buffer.alloc(20, '#')
+          const server = createServer(buffer)
+
+          expect(buffer.toString()).toStrictEqual('####################')
+          buffer.fill('?')
+          expect(buffer.toString()).toStrictEqual('????????????????????')
+
+          await makeFetch(server)('/favicon.ico')
+            .expectHeader('Content-Length', '20')
+            .expectHeader('Content-Type', 'image/x-icon')
+            .expectBody(Buffer.from('####################').toString())
+        })
+      })
+    })
   })
 })
